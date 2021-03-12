0066ca368ced61a8b1482d0d2c1ee1f3cfe6afaf
Scotlin White <scotlin23@hotmail.com>
2021-03-12 09:11:48 -0800

    fix likes issue pt.1

--- PART_2/app.py
+++ PART_2/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, flash, redirect, session, g
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
